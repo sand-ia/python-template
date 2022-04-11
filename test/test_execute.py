6af99d940e86269ccfec564d5fd7fae6b05e7d44
mikevive <mikevive327@gmail.com>
2022-04-11 17:24:18 -0500

    test added II

--- test/test_execute.py
+++ test/test_execute.py
@@ -1,11 +1,11 @@
 from src.application.service.service import Service
 
 
-def test_execute_():
+def test_execute_pass():
     service: Service = Service()
     assert service.execute() == 9
 
 
-def test_execute():
+def test_execute_fail():
     service: Service = Service()
     assert service.execute() != 8
